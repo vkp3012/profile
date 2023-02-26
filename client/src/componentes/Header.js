bca1a707d2360774b828b63c783b4176ed6ad80c
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-02-26 11:17:52 +0530

    dark mode theme is created

--- client/src/componentes/Header.js
+++ client/src/componentes/Header.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import DarkMode from './DarkMode';
 
 const Header = () => {
     return (
         <>
-            Header
+            <header className='header'>
+                <div>VKP</div>
+                <div>
+                    <h3>
+                        <Link 
+                            to="/"
+                        >
+                            Home
+                        </Link>
+                    </h3>
+                    <h3>
+                        <Link 
+                            to="/about"
+                        >
+                            About
+                        </Link>
+                    </h3>
+                    <h3>
+                        <Link 
+                            to="/contact"
+                        >
+                            Contact
+                        </Link>
+                    </h3>
+                </div>
+                <div>
+                    <h3>Login</h3>
+                    <DarkMode/>
+                </div>
+            </header>
         </>
     )
 }
